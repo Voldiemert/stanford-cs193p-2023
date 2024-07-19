8cb65a4370474e70093e43d6f994dc8a1b3c313c
Voldiemert <ryan.volmert@gmail.com>
2024-07-19 14:58:56 -0500

    Initial Commit

---
+++ stanford-cs193p-2023/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  stanford-cs193p-2023
+//
+//  Created by Ryan Volmert on 7/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ stanford-cs193p-2023/stanford_cs193p_2023App.swift
@@ -0,0 +1,17 @@
+//
+//  stanford_cs193p_2023App.swift
+//  stanford-cs193p-2023
+//
+//  Created by Ryan Volmert on 7/19/24.
+//
+
+import SwiftUI
+
+@main
+struct stanford_cs193p_2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
